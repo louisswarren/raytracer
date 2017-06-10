380695db053c6189bd1ca4615e033fad177710f3
Louis Warren <louis@lsw.nz>
2017-06-10 14:35:35 +1200

    Fix oversights

--- writebmp.c
+++ writebmp.c
@@ -4,21 +4,22 @@
 
 #include "writebmp.h"
 
-void writefileheader(FILE *f, size_t fsize)
+int writefileheader(FILE *f, size_t fsize)
 {
 	char     bfType[2]   = {'B', 'M'};
 	uint32_t bfSize      = fsize;
 	uint16_t bfReserved1 = 0;
 	uint16_t bfReserved2 = 0;
-	uint32_t bfOffBits   = 56;
+	uint32_t bfOffBits   = 54;
 	fwrite(bfType,       1, 2, f);
 	fwrite(&bfSize,      4, 1, f);
 	fwrite(&bfReserved1, 2, 1, f);
 	fwrite(&bfReserved2, 2, 1, f);
 	fwrite(&bfOffBits,   4, 1, f);
+	return 14;
 }
 
-void writeinfoheader(FILE *f, size_t width, size_t height)
+int writeinfoheader(FILE *f, size_t width, size_t height)
 {
 	uint32_t biSize          = 40;
 	uint32_t biWidth         = width;
@@ -42,6 +43,7 @@ void writeinfoheader(FILE *f, size_t width, size_t height)
 	fwrite(&biYPelsPerMeter, 4, 1, f);
 	fwrite(&biClrUsed,       4, 1, f);
 	fwrite(&biClrImportant,  4, 1, f);
+	return 40;
 }
 
 int writebitmap(FILE *f, char img[], size_t width, size_t height)
@@ -53,7 +55,7 @@ int writebitmap(FILE *f, char img[], size_t width, size_t height)
 	char pad[4] = {0, 0, 0, 0};
 	size_t pad_len = (width % 4) ? 4 - (width % 4) : 0;
 	for (int r = height - 1; r >= 0; r--) {
-		fwrite(img + width * r, 1, width, f);
+		fwrite(img + width * r * 3, 3, width, f);
 		if (pad_len)
 			fwrite(pad, 1, pad_len, f);
 	}
