aa12d5b76dc9957c5f2babc7c06902710d42399b
Louis Warren <louis@lsw.nz>
2017-06-12 20:32:56 +1200

    Make scene static

--- raytracer.c
+++ raytracer.c
@@ -31,15 +31,30 @@ typedef struct {
 	normal_func *normal;
 } Object;
 
+static Object scene[100];
+static size_t scene_ctr = 0;
+
+#define add_sphere(X) \
+	scene[scene_ctr++] = (Object){&X, &intersect_sphere, &normal_sphere}
+#define add_plane(X) \
+	scene[scene_ctr++] = (Object){&X, &intersect_plane, &normal_plane}
+#define add_coplane(X) \
+	scene[scene_ctr++] = (Object){&X, &intersect_coplane, &normal_plane}
+#define add_infinite_plane(X) \
+	scene[scene_ctr++] = (Object){&X, &intersect_infinite_plane, &normal_plane}
+
+
+
+
 double
-find_closest(Object *scene, size_t n, Vector pos, Vector dir, size_t *closest)
+find_closest(Vector pos, Vector dir, size_t *closest)
 {
 	void *drawable;
 	intersector *intersect;
 	double t;
 	double min_t = -1;
 	size_t argmin_t;
-	for (size_t i = 0; i < n; i++) {
+	for (size_t i = 0; i < scene_ctr; i++) {
 		drawable = scene[i].drawable;
 		intersect = scene[i].intersect;
 		t = intersect(drawable, pos, dir);
@@ -52,7 +67,7 @@ find_closest(Object *scene, size_t n, Vector pos, Vector dir, size_t *closest)
 	return min_t;
 }
 
-Color trace(Object *scene, size_t n, Vector pos, Vector dir)
+Color trace(Vector pos, Vector dir)
 {
 	Color ambient = {0.2, 0.2, 0.2};
 	Color color = {0, 0, 0};
@@ -60,7 +75,7 @@ Color trace(Object *scene, size_t n, Vector pos, Vector dir)
 	size_t closest_index;
 	Object *closest;
 	Shape *shape;
-	double t = find_closest(scene, n, pos, dir, &closest_index);
+	double t = find_closest(pos, dir, &closest_index);
 	if (t <= 0)
 		return color;
 
@@ -75,7 +90,7 @@ Color trace(Object *scene, size_t n, Vector pos, Vector dir)
 }
 
 void
-draw(Object *scene, size_t n)
+draw()
 {
 	Vector eye = {0, 0, -6};
 	int halfres = 200;
@@ -91,7 +106,7 @@ draw(Object *scene, size_t n)
 			Vector pix = {x, y, -5};
 			Vector dir = normalise(vectorsub(pix, eye));
 			size_t pt = (halfres - h - 1) * halfres * 2 + (halfres + w);
-			frame[pt] = trace(scene, n, eye, dir);
+			frame[pt] = trace(eye, dir);
 		}
 	}
 	FILE *f = fopen("output.bmp", "wb");
@@ -105,8 +120,11 @@ main(void)
 {
 	Color wallcolor = {1, 0.8, 0.4};
 	Color floorcolor = {0.3, 0.3, 0.35};
+
 	Sphere s1 = {COLOR_RED, {0, 0, 10}, 2};
 	Sphere s2 = {COLOR_BLUE, {4, 4, 5}, 1};
+	add_sphere(s1);
+	add_sphere(s2);
 
 	float d = 10;
 	float h = 2;
@@ -116,18 +134,14 @@ main(void)
 	Plane wall_left =   {COLOR_RED,  {-d, 0, 0},  {0, 0, 1}, {0, 1, 0}};
 	Plane wall_right =  {COLOR_BLUE, {d, 0, 0},   {0, 0, 1}, {0, 1, 0}};
 	Plane wall_bottom = {floorcolor, {0, -d, 0},  {1, 0, 0}, {0, 0, 1}};
+	add_infinite_plane(wall_back);
+	add_infinite_plane(wall_left);
+	add_infinite_plane(wall_right);
+	add_infinite_plane(wall_bottom);
 
 	Plane wall_top =    {floorcolor, {-h, d, r},  {h*2, 0, 0}, {0, 0, h*2}};
+	add_coplane(wall_top);
 
-	Object scene[] = {
-		{&s1, &intersect_sphere, &normal_sphere},
-		{&s2, &intersect_sphere, &normal_sphere},
-		{&wall_back,   &intersect_infinite_plane, &normal_plane},
-		{&wall_left,   &intersect_infinite_plane, &normal_plane},
-		{&wall_right,  &intersect_infinite_plane, &normal_plane},
-		{&wall_bottom, &intersect_infinite_plane, &normal_plane},
-		{&wall_top,    &intersect_coplane, &normal_plane},
-	};
-	draw(scene, 7);
+	draw();
 }
 
