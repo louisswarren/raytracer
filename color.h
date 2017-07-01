0c0f6e6c7918197c7a6f8b57fd383ecb3430007c
Louis Warren <louis@lsw.nz>
2017-07-01 12:59:40 +1200

    Add colorinterpolate to header

--- color.h
+++ color.h
@@ -4,6 +4,8 @@ typedef struct {
 
 Color coloradd(Color a, Color b, double b_scale);
 
+Color colorinterpolate(Color a, Color b, double t);
+
 Color phong(Color material, Color ambient, double diffuse, double specular);
 
 double colorvariance(Color expected, Color samples[], size_t n);
