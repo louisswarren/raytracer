6a489bf1e55fd0228780041bb5786ff59ce50b6b
Louis Warren <louis@lsw.nz>
2017-07-01 17:32:19 +1200

    Add missing declarations

--- geometry.h
+++ geometry.h
@@ -8,3 +8,13 @@ typedef struct {
 	Vector dir1, dir2;
 } Plane;
 
+Vector sphere_normal(Sphere *s, Vector *pos);
+
+double sphere_intersect(Sphere *s, Ray *ray);
+
+Vector plane_normal(Plane *p, Vector *pos);
+
+double infplane_intersect(Plane *p, Ray *ray);
+
+double plane_intersect(Plane *p, Ray *ray);
+
