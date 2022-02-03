9c25581a55c6b4a784354ad38cba7e32332bfb25
Louis Warren <louis@lsw.nz>
2022-02-04 12:54:54 +1300

    Json parsing

--- json.c
+++ json.c
@@ -6,6 +6,7 @@
 #define buf_is_lit(B, L) (sizeof(L) == (B).n + 1 && !memcmp((L), (B).p, (B).n))
 
 #define debug(...) fprintf(stderr, __VA_ARGS__)
+#define die(...) fprintf(stderr, __VA_ARGS__); exit(1)
 
 enum jtype {
 	JSTRING,
@@ -29,6 +30,35 @@ struct number {
 		double d;
 	};
 };
+
+struct vec {
+	double x, y, z;
+};
+
+struct col {
+	float r, g, b;
+};
+
+static struct {
+	struct {
+		int width, height;
+	} image;
+	struct {
+		struct vec pos, dir;
+	} perspective;
+	struct {
+		struct col col;
+		struct vec pos;
+	} lighting;
+	struct {
+		struct col col;
+		double distance, depth;
+	} fog;
+	struct {
+		double threshhold;
+	} antialiasing;
+} cfg;
+
 
 size_t
 get_strsz(const char *s)
@@ -99,7 +129,6 @@ void
 start_object(const char **s)
 {
 	assert(**s == '{');
-	debug("Started object\n");
 	(*s)++;
 	consume_whitespace(s);
 }
@@ -109,7 +138,6 @@ consume_string(const char **s)
 {
 	struct buffer b;
 	assert(**s == '"');
-	debug("Started string\n");
 	b.p = ++(*s);
 	b.n = get_strsz(*s);
 	*s += b.n + 1;
@@ -124,12 +152,13 @@ try_consume_key(const char **s, struct buffer *key)
 		(*s)++;
 		consume_whitespace(s);
 	}
-	if (**s == '}')
+	if (**s == '}') {
+		++(*s);
 		return 0;
+	}
 	*key = consume_string(s);
 	assert(**s == ':');
 	++(*s);
-	debug("Key consumed\n");
 	consume_whitespace(s);
 	return 1;
 }
@@ -174,37 +203,142 @@ consume_int(const char **s, int *x)
 	consume_whitespace(s);
 }
 
+void
+consume_vec(const char **s, struct vec *v)
+{
+	assert(**s == '[');
+	(*s)++;
+	consume_whitespace(s);
+	consume_double(s, &(v->x));
+	assert(**s == ',');
+	(*s)++;
+	consume_whitespace(s);
+	consume_double(s, &(v->y));
+	assert(**s == ',');
+	(*s)++;
+	consume_whitespace(s);
+	consume_double(s, &(v->z));
+	assert(**s == ']');
+	(*s)++;
+	consume_whitespace(s);
+}
+
+void
+consume_col(const char **s, struct col *c)
+{
+	int rx, gx, bx;
+	struct buffer b = consume_string(s);
+	sscanf(b.p, "#%02X%02X%02X", &rx, &gx, &bx);
+	c->r = rx / 255.0;
+	c->g = gx / 255.0;
+	c->b = bx / 255.0;
+}
+
+void
+parsecfg_antialiasing(const char **s)
+{
+	struct buffer key;
+	start_object(s);
+	while (try_consume_key(s, &key)) {
+		if (buf_is_lit(key, "threshhold")) {
+			consume_double(s, &cfg.antialiasing.threshhold);
+		} else {
+			die("Invalid option antialiasing->%.*s", (int) key.n, key.p);
+		}
+	}
+}
+
+void
+parsecfg_fog(const char **s)
+{
+	struct buffer key;
+	start_object(s);
+	while (try_consume_key(s, &key)) {
+		if (buf_is_lit(key, "colour")) {
+			consume_col(s, &cfg.fog.col);
+		} else if (buf_is_lit(key, "depth")) {
+			consume_double(s, &cfg.fog.depth);
+		} else if (buf_is_lit(key, "distance")) {
+			consume_double(s, &cfg.fog.distance);
+		} else {
+			die("Invalid option fog->%.*s", (int) key.n, key.p);
+		}
+	}
+}
+
+void
+parsecfg_image(const char **s)
+{
+	struct buffer key;
+	start_object(s);
+	while (try_consume_key(s, &key)) {
+		if (buf_is_lit(key, "width")) {
+			consume_int(s, &cfg.image.width);
+		} else if (buf_is_lit(key, "height")) {
+			consume_int(s, &cfg.image.height);
+		} else {
+			die("Invalid option image->%.*s", (int) key.n, key.p);
+		}
+	}
+}
+
+void
+parsecfg_lighting(const char **s)
+{
+	struct buffer key;
+	start_object(s);
+	while (try_consume_key(s, &key)) {
+		if (buf_is_lit(key, "position")) {
+			consume_vec(s, &cfg.lighting.pos);
+		} else if (buf_is_lit(key, "colour")) {
+			consume_col(s, &cfg.lighting.col);
+		} else {
+			die("Invalid option lighting->%.*s", (int) key.n, key.p);
+		}
+	}
+}
+
+void
+parsecfg_perspective(const char **s)
+{
+	struct buffer key;
+	start_object(s);
+	while (try_consume_key(s, &key)) {
+		if (buf_is_lit(key, "position")) {
+			consume_vec(s, &cfg.perspective.pos);
+		} else if (buf_is_lit(key, "direction")) {
+			consume_vec(s, &cfg.perspective.dir);
+		} else {
+			die("Invalid option perspective->%.*s", (int) key.n, key.p);
+		}
+	}
+}
+
 int
 main(void)
 {
 	char input[2048];
 	const char *s;
 	struct buffer key;
-	int width, height;
 
 	FILE *fp = fopen("scene.json", "r");
 	fread(input, 1, sizeof(input), fp);
 	s = input;
+
 	start_object(&s);
 	while (try_consume_key(&s, &key)) {
-		if (buf_is_lit(key, "image")) {
-			debug("Got key image\n");
-			start_object(&s);
-			while (try_consume_key(&s, &key)) {
-				if (buf_is_lit(key, "width")) {
-					consume_int(&s, &width);
-					printf("Width: %d\n", width);
-				} else if (buf_is_lit(key, "height")) {
-					consume_int(&s, &height);
-					printf("Height: %d\n", height);
-				}
-			}
-			break;
+		if (buf_is_lit(key, "antialiasing")) {
+			parsecfg_antialiasing(&s);
+		} else if (buf_is_lit(key, "fog")) {
+			parsecfg_fog(&s);
+		} else if (buf_is_lit(key, "image")) {
+			parsecfg_image(&s);
+		} else if (buf_is_lit(key, "lighting")) {
+			parsecfg_lighting(&s);
+		} else if (buf_is_lit(key, "perspective")) {
+			parsecfg_perspective(&s);
+		} else {
+			die("Invalid option %.*s", (int) key.n, key.p);
 		}
 	}
-	//printf("key: %s\n", buf);
-	//s++;
-	//s = j_whitespace(s);
-	//s = j_string(s, buf);
-	//printf("value: %s\n", buf);
 }
